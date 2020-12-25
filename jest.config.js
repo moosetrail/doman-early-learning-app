f18c833ab502d51ab97c4a997550cb7843102a4e
Johanna Hultén <min.mail@johannap.se>
2020-12-25 20:13:56 +0100

    Updated to use Jest for unit tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/projects/early-learning-web-app/tsconfig.spec.json',
+      stringifyContentPathRegex: "\\.html$",
+      astTransformers: {
+        before: [
+          "jest-preset-angular/build/InlineFilesTransformer",
+          "jest-preset-angular/build/StripStylesTransformer"
+        ]
+      }
+    }
+  }
+}
