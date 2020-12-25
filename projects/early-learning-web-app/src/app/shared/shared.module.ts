ee14970d49620c2999d042ddc413f9f577719457
Johanna Hultén <min.mail@johannap.se>
2020-12-25 20:49:30 +0100

    Create shared.module.ts

---
+++ projects/early-learning-web-app/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // VENDOR
+    CommonModule
+
+    // LOCAL
+  ],
+  exports: [
+    // VENDOR
+    CommonModule
+
+    // LOCAL
+  ]
+})
+export class SharedModule { }
