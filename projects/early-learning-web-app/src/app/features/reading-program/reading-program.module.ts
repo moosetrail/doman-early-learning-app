8f671424a1122b709ff4255d4569a223ce262a22
Johanna Hultén <min.mail@johannap.se>
2020-12-27 21:27:02 +0100

    Added reading program module

---
+++ projects/early-learning-web-app/src/app/features/reading-program/reading-program.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+
+import { ReadingProgramRoutingModule } from './reading-program-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    ReadingProgramRoutingModule
+  ]
+})
+export class ReadingProgramModule { }
