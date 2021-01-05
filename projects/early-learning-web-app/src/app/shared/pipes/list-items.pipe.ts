c12564fb23149ab69f691e555a2a7cbbe9736c12
Johanna Hultén <min.mail@johannap.se>
2021-01-05 22:27:34 +0100

    Update list-items.pipe.ts

--- projects/early-learning-web-app/src/app/shared/pipes/list-items.pipe.ts
+++ projects/early-learning-web-app/src/app/shared/pipes/list-items.pipe.ts
@@ -5,16 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ListItemsPipe implements PipeTransform {
 
-  transform(values: string[]): unknown {
+  transform(values: string[]): string {
 
     let str = '';
 
     for(let i = 0; i < values.length; i++){
 
       if(i+1 < values.length || i == 0){
-        str = str + values[i] + ' ';
+        if(i > 0){
+          str = str + ', ';
+        }
+        str = str + values[i];
       } else {
-        str = str + 'and ' + values[i];
+        str = str + ' and ' + values[i];
       }
     }
 
