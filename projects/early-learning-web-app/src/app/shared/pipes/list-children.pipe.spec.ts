cdfdc7c7557fcaf0fe8ee8db05319a325e0ed6c2
Johanna Hultén <min.mail@johannap.se>
2021-01-05 22:27:55 +0100

    Added ListChildren that lists childrens names

---
+++ projects/early-learning-web-app/src/app/shared/pipes/list-children.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ListChildrenPipe } from './list-children.pipe';
+
+describe('ListChildrenPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ListChildrenPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ projects/early-learning-web-app/src/app/shared/pipes/list-children.pipe.ts
@@ -0,0 +1,16 @@
+import { Inject, Pipe, PipeTransform } from '@angular/core';
+import { Child } from '../models/interfaces/child';
+import { ListItemsPipe } from './list-items.pipe';
+
+@Pipe({
+  name: 'listChildren'
+})
+export class ListChildrenPipe implements PipeTransform {
+
+  constructor(@Inject(ListItemsPipe) private listPipe: ListItemsPipe){}
+
+  transform(children: Child[]): string {
+    return this.listPipe.transform(children.map(child => child.name));
+  }
+
+}
